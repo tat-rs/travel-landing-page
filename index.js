5f120935a127fa591fc1bb0d19c885aaee3f16d6
Tatiana Roslyakova <tat-rs@yandex.ru>
2022-11-06 17:31:00 +0300

    feat: реализовать бургер меню

---
+++ index.js
@@ -0,0 +1,52 @@
+const page = document.querySelector(".page")
+const btnBurger = document.querySelector(".btn-burger");
+const btnsLines =  Array.from(document.querySelectorAll(".btn-burger__line"));
+const nav = document.querySelector(".nav-full");
+
+function open() {
+  btnBurger.classList.add("btn-burger_opened");
+  btnsLines[0].classList.add("btn-burger__line_left");
+  btnsLines[1].classList.add("btn-burger__line_hidden");
+  btnsLines[2].classList.add("btn-burger__line_right");
+
+  nav.classList.add("nav-full_opened");
+  page.style.overflow = 'hidden';
+  
+  nav.addEventListener("click", (evt) => closeOverlay(evt));
+  window.addEventListener("keydown", (evt) => closeByEsc(evt));
+}
+
+function close() {
+  btnBurger.classList.remove("btn-burger_opened")
+  btnsLines[0].classList.remove("btn-burger__line_left");
+  btnsLines[1].classList.remove("btn-burger__line_hidden");
+  btnsLines[2].classList.remove("btn-burger__line_right");
+
+  nav.classList.remove("nav-full_opened");
+  page.style.overflow = 'scroll';
+  
+  nav.removeEventListener("click", (evt) => closeOverlay(evt));
+  window.removeEventListener("keydown", (evt) => closeByEsc(evt));
+};
+
+function changeVisibleNav() {
+  if(btnBurger.classList.contains("btn-burger_opened")) {
+    close();
+  } else {
+    open();
+  }
+};
+
+function closeOverlay(evt) {
+  if (evt.target === evt.currentTarget) {
+    close();
+  }
+};
+
+function closeByEsc(evt) {
+  if(evt.key === "Escape") {
+    close();
+  }
+}
+
+btnBurger.addEventListener("click", () => changeVisibleNav());
